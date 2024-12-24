f5b207dcdb46948c86ba2d30dfa7790c76433068
ban04k4 <144313532+ban04k4@users.noreply.github.com>
2024-12-24 22:14:53 +0300

    added response models for using in backend

---
+++ backend/src/models/website.py
@@ -0,0 +1,59 @@
+from datetime import datetime
+from typing import List, Optional
+from pydantic import BaseModel, HttpUrl, Field
+from dataclasses import dataclass, field
+
+
+@dataclass
+class WebsiteHistoryEntry:
+    response_time: int
+    last_checked: datetime = field(default_factory=datetime.now)
+
+
+class WebsiteCheckBase(BaseModel):
+    url: HttpUrl
+    status: str
+    response_time: float
+    last_down: str
+    last_checked: datetime = Field(default_factory=datetime.now)
+    is_down: bool = False
+
+
+class WebsiteCheck(WebsiteCheckBase):
+    history: List[WebsiteHistoryEntry] = Field(default_factory=list)
+    _id: Optional[str] = None
+
+    def to_dict(self):
+        return {
+            "url": str(self.url),
+            "status": self.status,
+            "response_time": self.response_time,
+            "last_checked": self.last_checked,
+            "is_down": self.is_down,
+            "history": [
+                {
+                    "response_time": entry.response_time,
+                    "last_checked": entry.last_checked
+                }
+                for entry in self.history
+            ],
+            "last_down": self.last_down
+        }
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        print(data)
+        return cls(
+            url=data["url"],
+            status=data["status"],
+            response_time=data["response_time"],
+            last_down=data["last_down"],
+            last_checked=data["last_checked"],
+            is_down=data["is_down"],
+            history=data["history"],
+            _id=data.get("_id")
+        )
+
+class OutageReport(BaseModel):
+    name: str
+    reportCount: int
