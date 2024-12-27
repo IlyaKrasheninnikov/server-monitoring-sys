35e363138281a8835752243108a29640aa6f93b1
ban04k4 <144313532+ban04k4@users.noreply.github.com>
2024-12-27 13:00:30 +0300

    fix import

--- backend/src/models/website.py
+++ backend/src/models/website.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import List, Optional
 from pydantic import BaseModel, HttpUrl, Field
 from dataclasses import dataclass, field
